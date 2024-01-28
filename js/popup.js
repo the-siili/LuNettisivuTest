994e8dc93642bcad921d6dbd027e959b8bd7fb4e
the-siili <akseli.siili@gmail.com>
2024-01-28 16:22:02 +0200

    added photoviewer

--- js/popup.js
+++ js/popup.js
@@ -5,8 +5,22 @@ let imageBox = document.getElementById("openImg")
 
 
 function showImg(img){
-    var expandImg = document.getElementById("expandedImg");
-    expandImg.src = img
-    expandImg.parentElement.style.display = "block";
+    var items = [
+        {
+            src: img, // path to image
+            title: ' ' // If you skip it, there will display the original image name(image1)
+        }
+    ];
+
+// define options (if needed)
+    var options = {
+        // optionName: 'option value'
+        // for example:
+        index: 0, // this option means you will start at first image
+        draggable: false
+    };
+
+    // Initialize the plugin
+    var photoviewer = new PhotoViewer(items, options);
 
 }
