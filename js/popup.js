f82064b53632f3fbbdb5963bca981fb5a4b0347e
the-siili <akseli.siili@gmail.com>
2024-01-28 16:42:31 +0200

    Image viewer working. Add it to everything

--- js/popup.js
+++ js/popup.js
@@ -1,26 +1,46 @@
 
 
 
-let imageBox = document.getElementById("openImg")
+let mainImgPath = "img/portfolio/portfolio-"
+
+
+var items = [
+];
+
+
+
+
+
 
 
 function showImg(img){
-    var items = [
-        {
-            src: img, // path to image
-            title: ' ' // If you skip it, there will display the original image name(image1)
-        }
-    ];
 
-// define options (if needed)
+    // define options (if needed)
     var options = {
-        // optionName: 'option value'
-        // for example:
-        index: 0, // this option means you will start at first image
-        draggable: false
+    // optionName: 'option value'
+    // for example:
+    index: img-1, // this option means you will start at first image
+    draggable: false
     };
+
 
     // Initialize the plugin
     var photoviewer = new PhotoViewer(items, options);
 
 }
+
+
+
+window.onload = (event) => {
+    
+    for(var i = 1; i < 48; i++){
+        //console.log(mainImgPath + i + ".webp")
+        var newItem = {
+            src: mainImgPath + i + ".webp",
+            title: ' '
+        }
+        
+    
+        items.push(newItem)
+    }
+  };
